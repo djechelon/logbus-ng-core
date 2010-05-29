c66dea029e2705ed0d1e174bb248609edb57f7a1
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-05-29 23:01:48 +0000

    Initial revision of the core

---
+++ trunk/logbus-core/It.Unina.Dis.Logbus/IOutboundChannel.cs
@@ -0,0 +1,43 @@
+﻿/*
+ *                  Logbus-ng project
+ *    ©2010 Logbus Reasearch Team - Some rights reserved
+ *
+ *  Created by:
+ *      Vittorio Alfieri - vitty85@users.sourceforge.net
+ *      Antonio Anzivino - djechelon@users.sourceforge.net
+ *
+ *  Based on the research project "Logbus" by
+ *
+ *  Dipartimento di Informatica e Sistemistica
+ *  University of Naples "Federico II"
+ *  via Claudio, 21
+ *  80121 Naples, Italy
+ *
+ *  Software is distributed under Microsoft Reciprocal License
+ *  Documentation under Creative Commons 3.0 BY-SA License
+*/
+
+using It.Unina.Dis.Logbus.Filters;
+using System;
+namespace It.Unina.Dis.Logbus
+{
+    public interface IOutboundChannel
+        : IDisposable
+    {
+        string ID { get; set; }
+
+        string Name { get; set; }
+
+        string Description { get; set; }
+
+        void SubmitMessage(SyslogMessage message);
+
+        void Start();
+
+        void Stop();
+
+        IFilter Filter { get; set; }
+
+        IOutboundTransportFactory TransportFactory { get; set; }
+    }
+}
---
+++ trunk/logbus-core/It.Unina.Dis.Logbus/SyslogMessage.cs
@@ -0,0 +1,126 @@
+﻿/*
+ *                  Logbus-ng project
+ *    ©2010 Logbus Reasearch Team - Some rights reserved
+ *
+ *  Created by:
+ *      Vittorio Alfieri - vitty85@users.sourceforge.net
+ *      Antonio Anzivino - djechelon@users.sourceforge.net
+ *
+ *  Based on the research project "Logbus" by
+ *
+ *  Dipartimento di Informatica e Sistemistica
+ *  University of Naples "Federico II"
+ *  via Claudio, 21
+ *  80121 Naples, Italy
+ *
+ *  Software is distributed under Microsoft Reciprocal License
+ *  Documentation under Creative Commons 3.0 BY-SA License
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace It.Unina.Dis.Logbus
+{
+    /// <summary>
+    /// A syslog (RFC 5424) message
+    /// </summary>
+    /// <remarks>Currently, it can be serialized only into RFC5424 standard</remarks>
+    [Serializable()]
+    public struct SyslogMessage
+    {
+
+
+        public SyslogFacility Facility
+        {
+            get;
+            set;
+        }
+
+        public SyslogSeverity Severity
+        {
+            get;
+            set;
+        }
+
+        public DateTime? Timestamp
+        {
+            get;
+            set;
+        }
+
+        public int Version
+        {
+            get;
+            set;
+        }
+        public string Host
+        {
+            get;
+            set;
+        }
+
+
+        public string ApplicationName
+        {
+            get;
+            set;
+        }
+
+        public string ProcessID
+        {
+            get;
+            set;
+        }
+
+        public string MessageId
+        {
+            get;
+            set;
+        }
+
+        public IDictionary<string, string> Data
+        {
+            get;
+            set;
+        }
+
+
+        public string Text
+        {
+            get;
+            set;
+        }
+
+
+
+        public SyslogMessage(DateTime? timestamp, string host, SyslogFacility facility, SyslogSeverity level, string text, IDictionary<string, string> data)
+            : this()
+        {
+            Timestamp = timestamp;
+            Host = host;
+            Facility = facility;
+            Severity = level;
+            Text = text;
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            int prival = (int)Facility * 8 + (int)Severity;
+
+            throw new NotImplementedException();
+        }
+
+        public static SyslogMessage Parse(string payload)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static SyslogMessage Parse(byte[] payload)
+        {
+            return Parse(System.Text.Encoding.UTF8.GetString(payload));
+        }
+    }
+}
