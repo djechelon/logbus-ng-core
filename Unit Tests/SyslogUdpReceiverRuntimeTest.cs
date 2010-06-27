78685f5dc3e22b8fe2c1c1ccae059b1c2a7ca93f
/usr/local/ΕΨΗΕΛΩΝ <djechelon@users.noreply.github.com>
2010-06-27 23:41:24 +0000

    Created unit tests for SyslogUdpReceiver class

---
+++ Unit Tests/SyslogUdpReceiverRuntimeTest.cs
@@ -0,0 +1,98 @@
+﻿using It.Unina.Dis.Logbus.InChannels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using It.Unina.Dis.Logbus;
+using System.Threading;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Resources;
+using System.Reflection;
+
+namespace Unit_Tests
+{
+    /// <summary>
+    /// This test simulates real-world usage of SyslogUdpListener by submitting Syslog messages via a real UDP channel
+    /// </summary>
+    [TestClass()]
+    public class SyslogUdpReceiverRuntimeTest
+    {
+
+        private const int port = 37845;
+
+        private Thread injector_thread;
+        private AutoResetEvent test_finished;
+        private List<SyslogMessage> messages_to_test;
+        private int logs_sent = 0, logs_received = 0;
+
+        [TestInitialize()]
+        public void Init()
+        {
+            injector_thread = new Thread(Injector_Code);
+            injector_thread.IsBackground = true;
+            test_finished = new AutoResetEvent(false);
+
+            //Add messages
+            //messages_to_test = new List<SyslogMessage>();
+
+        }
+
+        [TestMethod()]
+        public void RuntimeTest()
+        {
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+                target.Configuration["port"] = port.ToString();
+                target.Configuration["ip"] = "127.0.0.1";
+                target.Start();
+                target.MessageReceived += new EventHandler<SyslogMessageEventArgs>(target_MessageReceived);
+
+                injector_thread.Start();
+                test_finished.WaitOne();
+
+                target.Stop();
+
+                //See if the number of sent logs matches the number of received logs
+                //Don't actually check correct parsing, just that they are parsed
+                Assert.AreEqual(logs_received, logs_sent);
+            }
+        }
+
+        void target_MessageReceived(object sender, SyslogMessageEventArgs e)
+        {
+            logs_received += 1;
+        }
+
+
+        private void Injector_Code()
+        {
+            try
+            {
+                //The injector will basically read Syslog messages, encoded in raw base64 form and collected with the proper tool by djechelon (or equivalent)
+                //The collector tool can be found at http://logbus-ng.svn.sourceforge.net/viewvc/logbus-ng/developers/djechelon/SyslogCollector/
+
+                IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, port);
+
+                using (StreamReader sr = new StreamReader(GetType().Assembly.GetManifestResourceStream("Unit_Tests.TestLogs.Syslog.base64.txt"), Encoding.GetEncoding(1252)))
+                {
+                    using (UdpClient client = new UdpClient())
+                        while (!sr.EndOfStream)
+                        {
+                            string base64line = sr.ReadLine();
+                            byte[] raw_log = Convert.FromBase64String(base64line);
+                            client.Send(raw_log, raw_log.Length, endpoint);
+                            logs_sent += 1;
+                        }
+                }
+
+            }
+            finally
+            {
+                test_finished.Set();
+            }
+        }
+
+    }
+}
---
+++ Unit Tests/SyslogUdpReceiverTest.cs
@@ -0,0 +1,291 @@
+﻿using It.Unina.Dis.Logbus.InChannels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using It.Unina.Dis.Logbus;
+
+namespace Unit_Tests
+{
+
+
+    /// <summary>
+    ///Classe di test per SyslogUdpReceiverTest.
+    ///Creata per contenere tutti gli unit test SyslogUdpReceiverTest
+    ///</summary>
+    [TestClass()]
+    public class SyslogUdpReceiverTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Ottiene o imposta il contesto dei test, che fornisce
+        ///funzionalità e informazioni sull'esecuzione dei test corrente.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Attributi di test aggiuntivi
+        // 
+        //Durante la scrittura dei test è possibile utilizzare i seguenti attributi aggiuntivi:
+        //
+        //Utilizzare ClassInitialize per eseguire il codice prima di eseguire il primo test della classe
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Utilizzare ClassCleanup per eseguire il codice dopo l'esecuzione di tutti i test di una classe
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Utilizzare TestInitialize per eseguire il codice prima di eseguire ciascun test
+        //
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //Utilizzare TestCleanup per eseguire il codice dopo l'esecuzione di ciascun test
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Test per Configuration
+        ///</summary>
+        [TestMethod()]
+        public void ConfigurationTest()
+        {
+            //Test 1: empty configuration
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+
+                try
+                {
+                    target.Start();
+                    Assert.Fail("Target not initialized but started");
+                }
+                catch (LogbusException)
+                {
+                    //OK
+                }
+                catch (Exception)
+                {
+                    Assert.Fail("I expected InvalidOperationException");
+                }
+            }
+
+            //Test 2: bad data
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+
+                try
+                {
+                    target.Configuration["port"] = "HELL0";
+                    target.Configuration["ip"] = "656.33.21.0";
+                    //target.Configuration.Add(new KeyValuePair<string, string>("port", "37888"));
+                    //target.Configuration.Add(new KeyValuePair<string, string>("ip", "127.0.0.1"));
+                    target.Start();
+                    Assert.Fail("It should never have started");
+                }
+                catch (LogbusException)
+                {
+                    //OK
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail("Listener should have started... [{0}]", ex.ToString());
+                }
+            }
+
+            //Test 3: IP and port filled (it should start)
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+
+                try
+                {
+                    target.Configuration["port"] = "37888";
+                    target.Configuration["ip"] = "127.0.0.1";
+                    //target.Configuration.Add(new KeyValuePair<string, string>("port", "37888"));
+                    //target.Configuration.Add(new KeyValuePair<string, string>("ip", "127.0.0.1"));
+                    target.Start();
+                    target.Stop();
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail("Listener should have started... [{0}]", ex.ToString());
+                }
+            }
+
+
+            //Test 4: start & stop twice
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+
+                try
+                {
+                    target.Configuration["port"] = "37888";
+                    target.Configuration["ip"] = "127.0.0.1";
+                    //target.Configuration.Add(new KeyValuePair<string, string>("port", "37888"));
+                    //target.Configuration.Add(new KeyValuePair<string, string>("ip", "127.0.0.1"));
+                    target.Start();
+                    target.Stop();
+                    target.Start();
+                    target.Stop();
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail("It shouldn't fail... [{0}]", ex.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        ///Test per Stop
+        ///</summary>
+        [TestMethod()]
+        public void StopTest()
+        {
+            //Test 1: stop but not started
+            try
+            {
+                using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+                {
+                    //target.Configuration["port"] = "37889";
+                    //target.Configuration["ip"] = "127.0.0.1";
+                    //target.Start();
+                    target.Stop();
+                }
+                Assert.Fail("I expected InvalidOperationException");
+            }
+            catch (InvalidOperationException)
+            {
+                //OK
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Something went wrong: [{0}]", ex);
+            }
+
+
+            //Test 2: start and stop
+            try
+            {
+                using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+                {
+                    target.Configuration["port"] = "37889";
+                    target.Configuration["ip"] = "127.0.0.1";
+                    target.Start();
+                    target.Stop();
+                }
+                //OK
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Something went wrong: [{0}]", ex);
+            }
+        }
+
+        /// <summary>
+        ///Test per Start
+        ///</summary>
+        [TestMethod()]
+        public void StartTest()
+        {
+            //Test 1: double start
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+                try
+                {
+                    target.Configuration["port"] = "37888";
+                    target.Configuration["ip"] = "127.0.0.1";
+                    //target.Configuration.Add(new KeyValuePair<string, string>("port", "37888"));
+                    //target.Configuration.Add(new KeyValuePair<string, string>("ip", "127.0.0.1"));
+                    target.Start();
+                    target.Start();
+                    Assert.Fail("It should have thrown LogbusException");
+                }
+                catch (LogbusException)
+                {
+                    //OK
+                }
+                catch (Exception)
+                {
+                    Assert.Fail("I expected LogbusException");
+                }
+            }
+
+            //OK test
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+                target.Configuration["port"] = "37889";
+                target.Configuration["ip"] = "127.0.0.1";
+                target.Start();
+            }
+            //OK
+        }
+
+        /// <summary>
+        ///Test per Costruttore SyslogUdpReceiver
+        ///</summary>
+        [TestMethod()]
+        public void SyslogUdpReceiverConstructorTest()
+        {
+            SyslogUdpReceiver target = new SyslogUdpReceiver();
+            Assert.IsNotNull(target);
+            Assert.IsInstanceOfType(target, typeof(IInboundChannel));
+        }
+
+
+        /// <summary>
+        ///Test per IpAddress
+        ///</summary>
+        [TestMethod()]
+        public void IpAddressTest()
+        {
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+                string expected = "127.0.0.1";
+                string actual;
+                target.IpAddress = expected;
+                actual = target.IpAddress;
+                Assert.AreEqual(expected, actual);
+                Assert.AreEqual(expected, target.Configuration["ip"]);
+            }
+        }
+
+        /// <summary>
+        ///Test per Port
+        ///</summary>
+        [TestMethod()]
+        public void PortTest()
+        {
+            using (SyslogUdpReceiver target = new SyslogUdpReceiver())
+            {
+                int expected = 8526;
+                int actual;
+                target.Port = expected;
+                actual = target.Port;
+                Assert.AreEqual(expected, actual);
+                Assert.AreEqual(expected.ToString(), target.Configuration["port"]);
+            }
+        }
+
+    }
+}
