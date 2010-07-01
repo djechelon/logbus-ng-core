64c333dc5529dfe7f5c434b5216c61e759c68163
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-07-01 23:07:14 +0000

    Preparing to test dynamic instantiation of objects through configuration

---
+++ trunk/logbus-core/Unit Tests/TestClasses/TestTransportFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using It.Unina.Dis.Logbus;
+
+namespace Unit_Tests.TestClasses
+{
+    /// <summary>
+    /// OK for NotImplementedException. This is a test
+    /// </summary>
+    public class TestTransportFactory
+        :IOutboundTransportFactory
+    {
+        #region IOutboundTransportFactory Membri di
+
+        public IOutboundTransport CreateTransport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetConfigurationParameter(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetConfigurationParameter(string key, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> Configuration
+        {
+            set { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+}
