602f87322ac2a3cdf3c09036b47be3aaf5f54471
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-07-01 15:32:12 +0000

    New test tool to parse XML configuration

---
+++ trunk/logbus-core/AppConfigParser/ConfigSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using It.Unina.Dis.Logbus.Configuration;
+using It.Unina.Dis.Logbus.Filters;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+
+namespace TestAppConfig
+{
+    class ConfigSerializer
+    {
+        public static void Main(string[] args)
+        {
+            LogbusCoreConfiguration config = new LogbusCoreConfiguration();
+
+            config.corefilter = new FacilityEqualsFilter() { facility = Facility.Kernel };
+
+            XmlSerializer seria = new XmlSerializer(typeof(LogbusCoreConfiguration));
+            seria.Serialize(Console.Out, config);
+            using (FileStream fs = new FileStream("output.txt", FileMode.CreateNew))
+            {
+                seria.Serialize(fs, config);
+            }
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
