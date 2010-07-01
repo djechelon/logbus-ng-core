fdde216162944098a339f80d7537c699c60a358a
Vitty85 <Vitty85@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-07-01 15:14:44 +0000

    Created a new UdpClient with ttl

---
+++ trunk/logbus-core/It.Unina.Dis.Logbus/OutTransports/UdpClientExpire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace It.Unina.Dis.Logbus.OutTransports
+{
+    class UdpClientExpire
+    {
+        public UdpClient Client
+        {
+            get;
+            set;
+        }
+
+        public DateTime? LastRefresh
+        {
+            get;
+            set;
+        }
+    }
+}
