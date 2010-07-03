6e0c94a207070d7191edaa6ba7f8ec73a0a4ebfb
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-07-03 17:21:25 +0000

    Started experimenting with Log4net. The following issues suggest us to reimplement RemoteSyslogAppender: *For a strange reason (might due to IPAddress.Parse method) UDPClient wants to use IPv6. Probably, the local endpoint is IPv4. This causes an Exception *RemoteSyslogAppender only supports RFC3164, we want 5424 *RFC3164 messages are not parsed by our tool (need to know where is the bug)
    Added a test case with a Log4net-generated RFC3164 message

---
+++ trunk/logbus-core/Log4test/RemoteSyslogAppenderTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using log4net;
+using log4net.Appender;
+using It.Unina.Dis.Logbus.InChannels;
+using System.Threading;
+
+namespace Log4test
+{
+    /// <summary>
+    /// Descrizione del riepilogo per RemoteSyslogAppenderTest
+    /// </summary>
+    [TestClass]
+    public class RemoteSyslogAppenderTest
+    {
+        public RemoteSyslogAppenderTest()
+        {
+            //
+            // TODO: aggiungere qui la logica del costruttore
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Ottiene o imposta il contesto del test che fornisce
+        ///le informazioni e le funzionalità per l'esecuzione del test corrente.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Attributi di test aggiuntivi
+        //
+        // È possibile utilizzare i seguenti attributi aggiuntivi per la scrittura dei test:
+        //
+        // Utilizzare ClassInitialize per eseguire il codice prima di eseguire il primo test della classe
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        // Utilizzare ClassCleanup per eseguire il codice dopo l'esecuzione di tutti i test della classe
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Utilizzare TestInitialize per eseguire il codice prima di eseguire ciascun test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Utilizzare TestCleanup per eseguire il codice dopo l'esecuzione di ciascun test
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        private ILog log;
+        private AutoResetEvent rcv_event;
+
+        [TestInitialize()]
+        public void Init()
+        {
+            //RemoteSyslogAppender appender = new RemoteSyslogAppender();
+            log = LogManager.GetLogger("ALL");
+            rcv_event = new AutoResetEvent(false);
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            using (SyslogUdpReceiver recv = new SyslogUdpReceiver())
+            {
+                recv.ParseError += new It.Unina.Dis.Logbus.ParseErrorEventHandler(recv_ParseError);
+                recv.MessageReceived += new It.Unina.Dis.Logbus.SyslogMessageEventHandler(recv_MessageReceived);
+                recv.Port = 3434;
+                recv.Start();
+                
+                log.Error("Test error message");
+
+                if (!rcv_event.WaitOne(2000)) Assert.Fail("Timed out");
+
+                //Nothing to assert :(
+            }
+        }
+
+        void recv_ParseError(object sender, It.Unina.Dis.Logbus.ParseErrorEventArgs e)
+        {
+            TestContext.WriteLine("Error receiving from Log4net: {0}", e.ExceptionObject);
+        }
+
+        void recv_MessageReceived(object sender, It.Unina.Dis.Logbus.SyslogMessageEventArgs e)
+        {
+            TestContext.WriteLine("Received message from Log4net: {0}", e.Message.ToRfc5424String());
+            rcv_event.Set();
+        }
+    }
+}
