03e41fe0307e49439359561fa2e87bff3838e943
/usr/local/ΕΨΗΕΛΩΝ <djechelon@users.noreply.github.com>
2010-07-04 23:10:08 +0000

    Latency test shows how long it takes for a message to traverse the bus

--- Unit Tests/LogbusSystemTest.cs
+++ Unit Tests/LogbusSystemTest.cs
@@ -11,6 +11,7 @@ using It.Unina.Dis.Logbus;
 using System.Threading;
 using System.Net.Sockets;
 using System.Net;
+using System.Globalization;
 
 namespace Unit_Tests
 {
@@ -186,44 +187,70 @@ namespace Unit_Tests
             }
         }
 
+        #region Test 1
+
         [TestInitialize()]
         public void Init()
         {
-            core_configuration = new LogbusCoreConfiguration[1];
-            core_configuration[0] = new LogbusCoreConfiguration();
-
-            //Only messages from Audit
-            core_configuration[0].corefilter = new FacilityEqualsFilter() { facility = Facility.Audit };
-
-
-            //Listening on UDP 3140 port
-            core_configuration[0].inchannels = new InboundChannelDefinition[1];
-            KeyValuePair[] prm = new KeyValuePair[1];
-            prm[0] = new KeyValuePair()
+            try
             {
-                name = "port",
-                value = t1_source_port.ToString()
-            };
-            core_configuration[0].inchannels[0] = new InboundChannelDefinition()
-            {
-                name = "udp-syslog",
-                type = "It.Unina.Dis.Logbus.InChannels.SyslogUdpReceiver, It.Unina.Dis.Logbus",
-                param = prm
-            };
+                core_configuration = new LogbusCoreConfiguration[2];
 
-            TestContext.WriteLine("Core configuration is rendered by the following markup");
-            using (MemoryStream ms = new MemoryStream())
-            {
-                new XmlSerializer(core_configuration[0].GetType()).Serialize(ms, core_configuration[0], core_configuration[0].xmlns);
-                ms.Flush();
-                ms.Seek(0, SeekOrigin.Begin);
-                TestContext.WriteLine(Encoding.Default.GetString(ms.ToArray()));
+                {
+                    core_configuration[0] = new LogbusCoreConfiguration();
+
+                    //Only messages from Audit
+                    core_configuration[0].corefilter = new FacilityEqualsFilter() { facility = Facility.Audit };
+
+
+                    //Listening on UDP 3140 port
+                    core_configuration[0].inchannels = new InboundChannelDefinition[1];
+                    KeyValuePair[] prm = new KeyValuePair[1];
+                    prm[0] = new KeyValuePair()
+                    {
+                        name = "port",
+                        value = t1_source_port.ToString()
+                    };
+                    core_configuration[0].inchannels[0] = new InboundChannelDefinition()
+                    {
+                        name = "udp-syslog",
+                        type = "It.Unina.Dis.Logbus.InChannels.SyslogUdpReceiver, It.Unina.Dis.Logbus",
+                        param = prm
+                    };
+                }
+
+                {
+                    core_configuration[1] = new LogbusCoreConfiguration();
+                    core_configuration[1].inchannels = new InboundChannelDefinition[1];
+
+                    KeyValuePair[] prm = new KeyValuePair[1];
+                    prm[0] = new KeyValuePair()
+                    {
+                        name = "port",
+                        value = t2_source_port.ToString()
+                    };
+                    core_configuration[1].inchannels[0] = new InboundChannelDefinition()
+                    {
+                        name = "udp-syslog",
+                        type = "It.Unina.Dis.Logbus.InChannels.SyslogUdpReceiver, It.Unina.Dis.Logbus",
+                        param = prm
+                    };
+                }
+
+                TestContext.WriteLine("Core configuration is rendered by the following markup");
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    new XmlSerializer(core_configuration[0].GetType()).Serialize(ms, core_configuration[0], core_configuration[0].xmlns);
+                    ms.Flush();
+                    ms.Seek(0, SeekOrigin.Begin);
+                    TestContext.WriteLine(Encoding.Default.GetString(ms.ToArray()));
+                }
+                TestContext.WriteLine("");
+
+                
+                
             }
-            TestContext.WriteLine("");
-
-            t1_step1 = new AutoResetEvent(false);
-            t1_step2 = new AutoResetEvent(false);
-            t1_finish = new AutoResetEvent(false);
+            catch (Exception ex) { Assert.Fail("Test failed: {0}", ex); }
         }
 
         private LogbusCoreConfiguration[] core_configuration;
@@ -234,25 +261,33 @@ namespace Unit_Tests
         [TestMethod]
         public void Test1()
         {
-            Thread source, client;
-            source = new Thread(t1_thread_Source);
-            client = new Thread(t1_thread_Client);
-            using (ILogBus logbus_service = new LogbusService(core_configuration[0]))
+            try
             {
-                logbus_service.Start();
-                client.Start(logbus_service);
+                t1_step1 = new AutoResetEvent(false);
+                t1_step2 = new AutoResetEvent(false);
+                t1_finish = new AutoResetEvent(false);
 
-                //Wait for client to be synchronized
-                t1_step1.WaitOne();
+                Thread source, client;
+                source = new Thread(t1_thread_Source);
+                client = new Thread(t1_thread_Client);
+                using (ILogBus logbus_service = new LogbusService(core_configuration[0]))
+                {
+                    logbus_service.Start();
+                    client.Start(logbus_service);
 
-                //Prepare source
-                source.Start();
-                //Wait for source to send all the messages
-                t1_step2.WaitOne();
+                    //Wait for client to be synchronized
+                    t1_step1.WaitOne();
 
-                //Wait for finish
-                t1_finish.WaitOne();
+                    //Prepare source
+                    source.Start();
+                    //Wait for source to send all the messages
+                    t1_step2.WaitOne();
+
+                    //Wait for finish
+                    t1_finish.WaitOne();
+                }
             }
+            catch (Exception ex) { Assert.Fail("Test failed: {0}", ex); }
         }
 
         private void t1_thread_Source()
@@ -305,5 +340,99 @@ namespace Unit_Tests
             }
             catch (Exception ex) { Assert.Fail("Test failed: {0}", ex); }
         }
+
+        #region Test 2 (latency)
+
+        private AutoResetEvent t2_step1, t2_step2, t2_finish;
+        private const int t2_source_port = 3142, t2_client_port = 3143;
+        private DateTime t2_start;
+
+        [TestMethod]
+        public void Test2()
+        {
+            try
+            {
+                t2_step1 = new AutoResetEvent(false);
+                t2_step2 = new AutoResetEvent(false);
+                t2_finish = new AutoResetEvent(false);
+
+                Thread source, client;
+                source = new Thread(t2_thread_Source);
+                client = new Thread(t2_thread_Client);
+                using (ILogBus logbus_service = new LogbusService(core_configuration[1]))
+                {
+                    logbus_service.Start();
+                    client.Start(logbus_service);
+
+                    //Wait for client to be synchronized
+                    t2_step1.WaitOne();
+
+                    //Prepare source
+                    source.Start();
+                    //Wait for source to send all the messages
+                    t2_step2.WaitOne();
+
+                    //Wait for finish
+                    t2_finish.WaitOne();
+                    //TestContext.
+                }
+            }
+            catch (Exception ex) { Assert.Fail("Test failed: {0}", ex); }
+        }
+
+        private void t2_thread_Source()
+        {
+            try
+            {
+                UdpClient client = new UdpClient();
+                IPEndPoint remote_ep = new IPEndPoint(IPAddress.Loopback, t2_source_port);
+
+
+                byte[] payload = Encoding.UTF8.GetBytes(MessagesToTest[0].ToRfc5424String());
+                t2_start = DateTime.Now;
+                client.Send(payload, payload.Length, remote_ep);
+
+
+                t2_step2.Set();
+            }
+            catch (Exception ex) { Assert.Fail("Test failed: {0}", ex); }
+        }
+
+        private void t2_thread_Client(object Logbus)
+        {
+            try
+            {
+                ILogbusController ctrl = Logbus as ILogbusController;
+                ctrl.CreateChannel("simple", "Simple", new TrueFilter(), "Very simple channel", 0);
+                Dictionary<string, string> input;
+                input = new Dictionary<string, string>();
+                IEnumerable<KeyValuePair<string, string>> output;
+                input.Add("ip", "127.0.0.1");
+                input.Add("port", t2_client_port.ToString());
+                string clientid = ctrl.SubscribeClient("simple", "udp", input, out output);
+                TestContext.WriteLine("Client ID obtained by logbus: {0}", clientid);
+
+                //Go ahead and send
+                t2_step1.Set();
+
+                //Only 6 messages expected
+                IPEndPoint remote_ep = new IPEndPoint(IPAddress.Any, 0);
+                using (UdpClient client = new UdpClient(t2_client_port))
+                {
+                    byte[] payload = client.Receive(ref remote_ep);
+                    TestContext.WriteLine("Time occurred for a message to traverse the bus: {0} milliseconds", (DateTime.Now - t2_start).TotalMilliseconds.ToString(CultureInfo.CurrentUICulture));
+                    SyslogMessage msg = SyslogMessage.Parse(payload);
+                }
+
+                ctrl.UnsubscribeClient(clientid);
+                t2_finish.Set();
+            }
+            catch (Exception ex) { Assert.Fail("Test failed: {0}", ex); }
+        }
+
+
+        #endregion
+
     }
+        #endregion
 }
