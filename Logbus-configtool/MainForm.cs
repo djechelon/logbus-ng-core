df73901b23a6fecd337ae4911b6b78befbd967cf
/usr/local/ΕΨΗΕΛΩΝ <djechelon@users.noreply.github.com>
2010-07-07 22:37:31 +0000

    Visual tool to aid final users create a proper App.config file for Logbus

---
+++ Logbus-configtool/MainForm.cs
@@ -0,0 +1,109 @@
+﻿/*
+ *                  Logbus-ng project
+ *    ©2010 Logbus Reasearch Team - Some rights reserved
+ *
+ *  Created by:
+ *      Vittorio Alfieri - vitty85@users.sourceforge.net
+ *      Antonio Anzivino - djechelon@users.sourceforge.net
+ *
+ *  Based on the research project "Logbus" by
+ *
+ *  Dipartimento di Informatica e Sistemistica
+ *  University of Naples "Federico II"
+ *  via Claudio, 21
+ *  80121 Naples, Italy
+ *
+ *  Software is distributed under Microsoft Reciprocal License
+ *  Documentation under Creative Commons 3.0 BY-SA License
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace It.Unina.Dis.Logbus.Configtool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Logbus-ng configuration tool.\n\nCopyright ©2010 the Logbus Research Team\nUniversity of Naples \"Federico II\" - Dipartimento di Informatica e Sistemistica\nvia Claudio, 21 - 80121 Naples, Italy", "About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateConfiguration();
+        }
+
+
+        private void UpdateConfiguration()
+        {
+            XmlDocument AppConfig = new XmlDocument();
+            AppConfig.AppendChild(AppConfig.CreateXmlDeclaration("1.0", "utf-8", string.Empty));
+
+            XmlElement configuration = AppConfig.CreateElement("configuration");
+            AppConfig.AppendChild(configuration);
+
+            XmlElement configsections = AppConfig.CreateElement("configSections");
+            configuration.AppendChild(configsections);
+
+            {
+                XmlElement configsection_core = AppConfig.CreateElement("section");
+                configsections.AppendChild(configsection_core);
+
+                XmlAttribute attr_section_core_name = AppConfig.CreateAttribute("name");
+                attr_section_core_name.Value = "logbus-core";
+                configsection_core.Attributes.Append(attr_section_core_name);
+
+                XmlAttribute attr_section_core_type = AppConfig.CreateAttribute("type");
+                attr_section_core_type.Value = typeof(It.Unina.Dis.Logbus.Configuration.LogbusCoreConfigurationSectionHandler).AssemblyQualifiedName;
+                configsection_core.Attributes.Append(attr_section_core_type);
+            }
+
+            {
+                XmlElement configsection_client = AppConfig.CreateElement("section");
+                configsections.AppendChild(configsection_client);
+
+                XmlAttribute attr_section_core_name = AppConfig.CreateAttribute("name");
+                attr_section_core_name.Value = "logbus-client";
+                configsection_client.Attributes.Append(attr_section_core_name);
+
+                XmlAttribute attr_section_core_type = AppConfig.CreateAttribute("type");
+                attr_section_core_type.Value = typeof(It.Unina.Dis.Logbus.Configuration.LogbusClientConfigurationSectionHandler).AssemblyQualifiedName;
+                configsection_client.Attributes.Append(attr_section_core_type);
+            }
+
+            {
+                XmlElement configsection_source = AppConfig.CreateElement("section");
+                configsections.AppendChild(configsection_source);
+
+                XmlAttribute attr_section_core_name = AppConfig.CreateAttribute("name");
+                attr_section_core_name.Value = "logbus-source";
+                configsection_source.Attributes.Append(attr_section_core_name);
+
+                XmlAttribute attr_section_core_type = AppConfig.CreateAttribute("type");
+                attr_section_core_type.Value = typeof(It.Unina.Dis.Logbus.Configuration.LogbusSourceConfigurationSectionHandler).AssemblyQualifiedName;
+                configsection_source.Attributes.Append(attr_section_core_type);
+            }
+
+            new XmlShowForm() { Node = AppConfig }.Show(this);
+        }
+
+    }
+}
---
+++ Logbus-configtool/XmlShowForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace It.Unina.Dis.Logbus.Configtool
+{
+    partial class XmlShowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.txtXml = new System.Windows.Forms.TextBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(984, 24);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // txtXml
+            // 
+            this.txtXml.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtXml.Location = new System.Drawing.Point(0, 24);
+            this.txtXml.Multiline = true;
+            this.txtXml.Name = "txtXml";
+            this.txtXml.Size = new System.Drawing.Size(984, 740);
+            this.txtXml.TabIndex = 1;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // XmlShowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 764);
+            this.Controls.Add(this.txtXml);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "XmlShowForm";
+            this.Text = "XmlShowForm";
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtXml;
+    }
+}
