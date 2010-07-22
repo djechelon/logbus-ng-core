9d0c3c7188d9f2294f1783ce80ce1af353211227
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-07-22 20:30:39 +0000

    Cloning samples to the trunk

---
+++ trunk/logbus-core/Samples/DemoWarningClient/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ *                  Logbus-ng project
+ *    ©2010 Logbus Reasearch Team - Some rights reserved
+ *
+ *  Created by:
+ *      Vittorio Alfieri - vitty85@users.sourceforge.net
+ *      Antonio Anzivino - djechelon@users.sourceforge.net
+ *
+ *  Based on the research project "Logbus" by
+ *
+ *  Dipartimento di Informatica e Sistemistica
+ *  University of Naples "Federico II"
+ *  via Claudio, 21
+ *  80121 Naples, Italy
+ *
+ *  Software is distributed under Microsoft Reciprocal License
+ *  Documentation under Creative Commons 3.0 BY-SA License
+*/
+
+using System;
+using It.Unina.Dis.Logbus.Filters;
+using It.Unina.Dis.Logbus.Clients;
+using System.Threading;
+
+namespace DemoWarningClient
+{
+    class Program
+    {
+        private static ILogClient logclient;
+
+        static void Main(string[] args)
+        {
+            SeverityFilter warning_filter = new SeverityFilter()
+            {
+                comparison = ComparisonOperator.geq,
+                severity = Severity.Warning
+            };
+
+            Console.WriteLine("This program is going to listen for all messages with severity >= Warning.");
+            Console.WriteLine("All received messages will be written on this console in their native format.");
+
+            Console.WriteLine();
+            Console.WriteLine("You must start Logbus daemon prior to running this program.");
+            Console.WriteLine("Please press ENTER when you are sure that Logbus daemon is already running...");
+            Console.ReadLine();
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
+
+            try
+            {
+                logclient = ClientHelper.CreateDefaultClient(warning_filter);
+                logclient.MessageReceived += new It.Unina.Dis.Logbus.SyslogMessageEventHandler(logclient_MessageReceived);
+
+                logclient.Start();
+                Console.WriteLine("Listener is started. Press CTRL+C to exit when you are finished");
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong...");
+                Console.WriteLine(ex);
+                Environment.Exit(1);
+            }
+        }
+
+        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            //Shut down
+            if (logclient != null)
+                logclient.Dispose();
+            Console.WriteLine("Program shutting down");
+            Environment.Exit(0);
+        }
+
+        static void logclient_MessageReceived(object sender, It.Unina.Dis.Logbus.SyslogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.ToString());
+        }
+    }
+}
---
+++ trunk/logbus-core/Samples/RandomMessagePump/Program.cs
@@ -0,0 +1,113 @@
+﻿/*
+ *                  Logbus-ng project
+ *    ©2010 Logbus Reasearch Team - Some rights reserved
+ *
+ *  Created by:
+ *      Vittorio Alfieri - vitty85@users.sourceforge.net
+ *      Antonio Anzivino - djechelon@users.sourceforge.net
+ *
+ *  Based on the research project "Logbus" by
+ *
+ *  Dipartimento di Informatica e Sistemistica
+ *  University of Naples "Federico II"
+ *  via Claudio, 21
+ *  80121 Naples, Italy
+ *
+ *  Software is distributed under Microsoft Reciprocal License
+ *  Documentation under Creative Commons 3.0 BY-SA License
+*/
+
+using System;
+using System.Threading;
+using It.Unina.Dis.Logbus.Loggers;
+
+namespace RandomMessagePump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
+            Console.WriteLine("This program is going to submit random log messages to Logbus-ng.");
+            Console.WriteLine("You should have already started Logbus daemon, otherwise you will lose messages.");
+            Console.WriteLine("Press ENTER when you are ready to begin...");
+
+            Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Let's start! Random messages are going to be sent to Logbus-ng. Press CTRL+C when you are finished.");
+
+            try
+            {
+                ILog the_log = LoggerHelper.CreateDefaultLogger();
+
+                while (true)
+                {
+                    Random rand = new Random();
+                    Thread.Sleep(rand.Next(5000, 15000)); //Random sleep between 5 and 15 seconds
+
+                    switch (rand.Next(0, 10))
+                    {
+                        case 0:
+                            {
+                                the_log.Debug("A debug message");
+                                break;
+                            }
+                        case 1:
+                            {
+                                the_log.Notice("A notice message");
+                                break;
+                            }
+                        case 2:
+                            {
+                                the_log.Info("An informational message");
+                                break;
+                            }
+                        case 3:
+                            {
+                                the_log.Warning("A warning message");
+                                break;
+                            }
+                        case 4:
+                            {
+                                the_log.Error("An error message");
+                                break;
+                            }
+                        case 5:
+                            {
+                                the_log.Alert("An alert message");
+                                break;
+                            }
+                        case 6:
+                            {
+                                the_log.Critical("A critical message");
+                                break;
+                            }
+                        case 7:
+                            {
+                                the_log.Emergency("An emergency message");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Not logging this time");
+                                break;
+                            }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong...");
+                Console.WriteLine(ex);
+                Environment.Exit(1);
+            }
+        }
+
+        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+    }
+}
