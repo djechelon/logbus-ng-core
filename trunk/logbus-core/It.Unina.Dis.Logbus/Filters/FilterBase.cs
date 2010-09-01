6c12e7fa3e389a067525cb6503705873de948c4a
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-09-01 17:34:49 +0000

    Implemented binary operators for filters

--- trunk/logbus-core/It.Unina.Dis.Logbus/Filters/FilterBase.cs
+++ trunk/logbus-core/It.Unina.Dis.Logbus/Filters/FilterBase.cs
@@ -61,5 +61,68 @@ namespace It.Unina.Dis.Logbus.Filters
         public abstract bool IsMatch(SyslogMessage message);
 
         #endregion
+
+        /// <summary>
+        /// Operator implementing the logical <b>not</b> of a filter
+        /// </summary>
+        /// <param name="filter">Filter to negate</param>
+        /// <returns>A new instance of a <see cref="It.Unina.Dis.Logbus.Filters.NotFilter"/> that negates output of previous filter</returns>
+        public static FilterBase operator !(FilterBase filter)
+        {
+            if (filter == null) throw new System.ArgumentNullException("filter");
+            return new NotFilter() { filter = filter };
+        }
+
+        /// <summary>
+        /// Operator implementing the logical <b>or</b> of a filter
+        /// </summary>
+        /// <param name="a">Left-side filter</param>
+        /// <param name="b">Right-side filter</param>
+        /// <returns>A new instance of a <see cref="It.Unina.Dis.Logbus.Filters.OrFilter"/> with the two filters as arguments</returns>
+        /// <remarks>If one of the original filters is an <b>or</b> of other filters, it gets merged with the other one. It means that two
+        /// or filters won't result in an "outer" or containing both, but into a single or filter. This might affect advanced debugging but
+        /// does never affect the final result of message processing</remarks>
+        public static FilterBase operator |(FilterBase a, FilterBase b)
+        {
+            if (a == null) throw new System.ArgumentNullException("a");
+            if (b == null) throw new System.ArgumentNullException("b");
+
+            FilterBase[] a_array = null, b_array = null;
+
+            a_array = (a is OrFilter) ? ((OrFilter)a).filter : new FilterBase[] { a };
+            b_array = (b is OrFilter) ? ((OrFilter)b).filter : new FilterBase[] { b };
+
+            FilterBase[] final_array = new FilterBase[a_array.Length + b_array.Length];
+            System.Array.Copy(a_array, 0, final_array, 0, a_array.Length);
+            System.Array.Copy(b_array, 0, final_array, a_array.Length, b_array.Length);
+
+            return new OrFilter() { filter = final_array };
+        }
+
+        /// <summary>
+        /// Operatand implementing the logical <b>and</b> of a filter
+        /// </summary>
+        /// <param name="a">Left-side filter</param>
+        /// <param name="b">Right-side filter</param>
+        /// <returns>A new instance of a <see cref="It.Unina.Dis.Logbus.Filters.AndFilter"/> with the two filters as arguments</returns>
+        /// <remarks>If one of the andiginal filters is an <b>and</b> of other filters, it gets merged with the other one. It means that two
+        /// and filters won't result in an "outer" and containing both, but into a single and filter. This might affect advanced debugging but
+        /// does never affect the final result of message processing</remarks>
+        public static FilterBase operator &(FilterBase a, FilterBase b)
+        {
+            if (a == null) throw new System.ArgumentNullException("a");
+            if (b == null) throw new System.ArgumentNullException("b");
+
+            FilterBase[] a_array = null, b_array = null;
+
+            a_array = (a is AndFilter) ? ((AndFilter)a).filter : new FilterBase[] { a };
+            b_array = (b is AndFilter) ? ((AndFilter)b).filter : new FilterBase[] { b };
+
+            FilterBase[] final_array = new FilterBase[a_array.Length + b_array.Length];
+            System.Array.Copy(a_array, 0, final_array, 0, a_array.Length);
+            System.Array.Copy(b_array, 0, final_array, a_array.Length, b_array.Length);
+
+            return new AndFilter() { filter = final_array };
+        }
     }
 }
