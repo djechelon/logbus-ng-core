ffaba71411bec6196215f76bce92947a21b3e74f
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-09-01 17:30:15 +0000

    Restored inheritance from XmlnsSupport

--- trunk/logbus-core/It.Unina.Dis.Logbus/Configuration/LogbusCoreConfiguration.cs
+++ trunk/logbus-core/It.Unina.Dis.Logbus/Configuration/LogbusCoreConfiguration.cs
@@ -27,7 +27,7 @@ namespace It.Unina.Dis.Logbus.Configuration
     [System.ComponentModel.DesignerCategoryAttribute("code")]
     [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://www.dis.unina.it/logbus-ng/configuration")]
     [System.Xml.Serialization.XmlRootAttribute("logbus-core", Namespace = "http://www.dis.unina.it/logbus-ng/configuration", IsNullable = false)]
-    public partial class LogbusCoreConfiguration
+    public partial class LogbusCoreConfiguration : XmlnsSupport
     {
 
         private InboundChannelDefinition[] inchannelsField;
