38000a4efe0f6094258ebfe278083b8f186cf848
/usr/local/ΕΨΗΕΛΩΝ <djechelon@users.noreply.github.com>
2010-09-16 14:46:22 +0000

    Implemented data access in EntityPlugin

--- It.Unina.Dis.Logbus.Extensions/Entities/EntityPlugin.cs
+++ It.Unina.Dis.Logbus.Extensions/Entities/EntityPlugin.cs
@@ -18,46 +18,182 @@
 */
 
 using System;
+using System.Data;
+using It.Unina.Dis.Logbus.Utils;
+using System.Threading;
 namespace It.Unina.Dis.Logbus.Entities
 {
+    /// <summary>
+    /// Plugin that manages Logbus-ng entities, where the entity is user-defined by configuration
+    /// </summary>
     public sealed class EntityPlugin
-        :IPlugin
+        : IPlugin
     {
+        private ILogBus logbus;
+        private BlockingFifoQueue<SyslogMessage> message_queue;
+        private Thread worker_thread;
+
+        private DataColumn colHost, colProc, colModule, colLogger, colClass, colMethod, colFFDA, colLastAction;
+        private UniqueConstraint primary_key;
+        private DataTable entity_table;
+
+        #region Constructor/Destructor
+
+        public EntityPlugin()
+        {
+            message_queue = new BlockingFifoQueue<SyslogMessage>();
+
+            worker_thread = new Thread(WorkerLoop);
+            worker_thread.IsBackground = true;
+            worker_thread.Name = "EntityPlugin.WorkerLoop";
+            worker_thread.Start();
+
+            entity_table = new DataTable("Entities");
+            colHost = new DataColumn("Host", typeof(string))
+            {
+                AllowDBNull = true,
+                ReadOnly = true,
+                Unique = false
+            };
+            colProc = new DataColumn("Process", typeof(string))
+            {
+                AllowDBNull = true,
+                ReadOnly = true,
+                Unique = false
+            };
+            colModule = new DataColumn("Module", typeof(string))
+            {
+                AllowDBNull = true,
+                ReadOnly = true,
+                Unique = false
+            };
+            colLogger = new DataColumn("Logger", typeof(string))
+            {
+                AllowDBNull = true,
+                ReadOnly = true,
+                Unique = false
+            };
+            colClass = new DataColumn("Class", typeof(string))
+            {
+                AllowDBNull = true,
+                ReadOnly = true,
+                Unique = false
+            };
+            colMethod = new DataColumn("Method", typeof(string))
+            {
+                AllowDBNull = true,
+                ReadOnly = true,
+                Unique = false
+            };
+            colFFDA = new DataColumn("FFDA", typeof(bool))
+            {
+                AllowDBNull = false,
+                DefaultValue = false,
+                ReadOnly = false,
+                Unique = false
+            };
+            colLastAction = new DataColumn("LastAction", typeof(DateTime))
+            {
+                AllowDBNull = false,
+                ReadOnly = false,
+                Unique = false,
+                DateTimeMode = DataSetDateTime.Utc,
+            };
+
+            entity_table.Columns.Add(colHost);
+            entity_table.Columns.Add(colProc);
+            entity_table.Columns.Add(colModule);
+            entity_table.Columns.Add(colLogger);
+            entity_table.Columns.Add(colClass);
+            entity_table.Columns.Add(colMethod);
+            entity_table.Columns.Add(colFFDA);
+            entity_table.Columns.Add(colLastAction);
+
+            primary_key = new UniqueConstraint(new DataColumn[] { colHost, colProc, colModule, colLogger, colClass, colMethod }, true)
+            {
+                ConstraintName = "Primary"
+            };
+            entity_table.Constraints.Add(primary_key);
+        }
+
+        ~EntityPlugin()
+        {
+            Dispose(false);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            worker_thread.Abort();
+
+            if (disposing)
+            {
+                message_queue.Dispose();
+            }
+
+            message_queue = null;
+
+            Disposed = true;
+        }
+
+
+        private bool Disposed;
+        #endregion
+
         #region IPlugin Membri di
 
+        /// <summary>
+        /// Implements IPlugin.Register
+        /// </summary>
         public void Register(ILogBus logbus)
         {
+            if (Disposed) throw new ObjectDisposedException(GetType().FullName);
+
+            this.logbus = logbus;
             logbus.MessageReceived += new EventHandler<SyslogMessageEventArgs>(logbus_MessageReceived);
         }
 
-        void logbus_MessageReceived(object sender, SyslogMessageEventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
+        /// <summary>
+        /// Implements IPlugin.Unregister
+        /// </summary>
         public void Unregister()
         {
-            throw new System.NotImplementedException();
+            if (Disposed) throw new ObjectDisposedException(GetType().FullName);
+
+            logbus.MessageReceived -= logbus_MessageReceived;
         }
 
+        /// <summary>
+        /// Implements ILogSupport.Log
+        /// </summary>
         public It.Unina.Dis.Logbus.Loggers.ILog Log
         {
             private get;
             set;
         }
 
+        /// <summary>
+        /// Implements IPlugin.Name
+        /// </summary>
         public string Name
         {
             get { return "Logbus.EntityManager"; }
         }
 
+        /// <summary>
+        /// Implements IPlugin.GetWsdlSkeletons
+        /// </summary>
         public WsdlSkeletonDefinition[] GetWsdlSkeletons()
         {
+            if (Disposed) throw new ObjectDisposedException(GetType().FullName);
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Implements IPlugin.GetPluginRoot
+        /// </summary>
         public System.MarshalByRefObject GetPluginRoot()
         {
+            if (Disposed) throw new ObjectDisposedException(GetType().FullName);
             throw new System.NotImplementedException();
         }
 
@@ -65,11 +201,74 @@ namespace It.Unina.Dis.Logbus.Entities
 
         #region IDisposable Membri di
 
+        /// <summary>
+        /// Implements IDisposable.Dispose
+        /// </summary>
         public void Dispose()
         {
+            if (Disposed) throw new ObjectDisposedException(GetType().FullName);
             throw new System.NotImplementedException();
         }
 
         #endregion
+
+        private void logbus_MessageReceived(object sender, SyslogMessageEventArgs e)
+        {
+            message_queue.Enqueue(e.Message);
+        }
+
+        private void WorkerLoop()
+        {
+            try
+            {
+                while (true)
+                {
+                    SyslogMessage message = message_queue.Dequeue();
+                    SyslogAttributes attrs = message.GetAdvancedAttributes();
+
+                    bool ffda = message.MessageId == "FFDA";
+                    try
+                    {
+                        try
+                        {
+
+                            entity_table.Rows.Add(
+                                message.Host,
+                                (message.ProcessID != null) ? message.ProcessID : message.ApplicationName,
+                                attrs.ModuleName,
+                                attrs.LogName,
+                                attrs.ClassName,
+                                attrs.MethodName,
+                                ffda,
+                                message.Timestamp
+                                );
+                        }
+                        catch (ConstraintException)
+                        {
+                            //We suppose we are trying to insert a duplicate primary key, then now we switch to update
+                            DataRow existing_row = entity_table.Rows.Find(message.Host,
+                                (message.ProcessID != null) ? message.ProcessID : message.ApplicationName,
+                                attrs.ModuleName,
+                                attrs.LogName,
+                                attrs.ClassName,
+                                attrs.MethodName);
+
+                            existing_row.BeginEdit();
+                            existing_row[colFFDA] = ffda;
+                            existing_row[colLastAction] = message.Timestamp;
+                            existing_row.EndEdit();
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("Unable to add an entity row into the data table");
+                        Log.Debug(string.Format("Error details: {0}", ex.Message));
+                    }
+                }
+            }
+            catch (ThreadAbortException) { }
+        }
+
     }
 }
