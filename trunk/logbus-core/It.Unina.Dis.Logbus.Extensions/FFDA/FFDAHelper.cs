5075e7315b6a2c09da5e130b702f9d5bdde760c9
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-10-22 09:46:16 +0000

    Fixed a bug that prevented the instrumented logger to be instantiated if no (redundant) logger-specific configuration was specified. Implemented missing constructor Added new reliable constructor for FFDA logger Added documentation

--- trunk/logbus-core/It.Unina.Dis.Logbus.Extensions/FFDA/FFDAHelper.cs
+++ trunk/logbus-core/It.Unina.Dis.Logbus.Extensions/FFDA/FFDAHelper.cs
@@ -17,9 +17,7 @@
  *  Documentation under Creative Commons 3.0 BY-SA License
 */
 
-using System;
 using It.Unina.Dis.Logbus.Collectors;
-using It.Unina.Dis.Logbus.Loggers;
 using System.Net;
 namespace It.Unina.Dis.Logbus.FFDA
 {
@@ -39,22 +37,81 @@ namespace It.Unina.Dis.Logbus.FFDA
         /// <exception cref="System.InvalidOperationException">Configuration is not set or is invalid</exception>
         public static IFFDALogger CreateFFDALogger(string loggerName)
         {
-            return new FFDALogger(CollectorHelper.CreateCollector(loggerName));
+            ILogCollector collector;
+            try
+            {
+                collector = CollectorHelper.CreateCollectorForLogger(loggerName);
+            }
+            catch
+            {
+                collector = CollectorHelper.CreateCollector();
+            }
+            return new FFDALogger(collector) { LogName = loggerName };
         }
 
-        public  static IFFDAMonitorLogger CreateFFDAMonitorLogger()
+        /// <summary>
+        /// Creates the FFDA logger for monitors
+        /// </summary>
+        /// <returns></returns>
+        public static IFFDAMonitorLogger CreateFFDAMonitorLogger()
         {
             return new FFDAMonitorLogger(CollectorHelper.CreateCollector());
         }
 
+        /// <summary>
+        /// Creates an instrumentation logger. It can perform both regular logging and FFD logging
+        /// </summary>
+        /// <param name="loggerName"></param>
+        /// <returns></returns>
         public static IInstrumentedLogger CreateInstrumentedLogger(string loggerName)
         {
-            return new FFDALogger(CollectorHelper.CreateCollector(loggerName));
+            ILogCollector collector;
+            try
+            {
+                collector = CollectorHelper.CreateCollectorForLogger(loggerName);
+            }
+            catch
+            {
+                collector = CollectorHelper.CreateCollector();
+            }
+            return new FFDALogger(collector) { LogName = loggerName };
         }
 
+        /// <summary>
+        /// Creates an unreliable FFD logger
+        /// </summary>
+        /// <param name="loggerName">Name of logger</param>
+        /// <param name="host">Destination host</param>
+        /// <param name="port">Destination port</param>
+        /// <returns>An FFD logger that works on unreliable transport</returns>
+        public static IFFDALogger CreateUnreliableFFDALogger(string loggerName, string host, int port)
+        {
+            return new FFDALogger(CollectorHelper.CreateUnreliableCollector(host, port)) { LogName = loggerName };
+        }
+
+        /// <summary>
+        /// Creates an unreliable FFD logger
+        /// </summary>
+        /// <param name="loggerName">Name of logger</param>
+        /// <param name="host">Destination host</param>
+        /// <param name="port">Destination port</param>
+        /// <returns>An FFD logger that works on unreliable transport</returns>
         public static IFFDALogger CreateUnreliableFFDALogger(string loggerName, IPAddress host, int port)
         {
-            throw new NotImplementedException();
+            return new FFDALogger(CollectorHelper.CreateUnreliableCollector(host, port)) { LogName = loggerName };
+        }
+
+        /// <summary>
+        /// Creates a reliable FFD logger
+        /// </summary>
+        /// <param name="loggerName">Name of logger</param>
+        /// <param name="host">Destination host</param>
+        /// <param name="port">Destination port</param>
+        /// <returns>An FFD logger that works on reliable transport</returns>
+        /// <remarks>Log messages are not subject to loss, however this may affect performance</remarks>
+        public static IFFDALogger CreateReliableFFDALogger(string loggerName, string host, int port)
+        {
+            return new FFDALogger(CollectorHelper.CreateUnreliableCollector(host, port)) { LogName = loggerName };
         }
     }
 }
