d85599ddd0dba67f6200353366ac074243d28a28
/usr/local/ΕΨΗΕΛΩΝ <djechelon@users.noreply.github.com>
2010-10-26 12:13:07 +0000

    FFDA information, with extension to SyslogMessage

---
+++ It.Unina.Dis.Logbus.Extensions/FFDA/FFDAInformation.cs
@@ -0,0 +1,98 @@
+﻿/*
+ *                  Logbus-ng project
+ *    ©2010 Logbus Reasearch Team - Some rights reserved
+ *
+ *  Created by:
+ *      Vittorio Alfieri - vitty85@users.sourceforge.net
+ *      Antonio Anzivino - djechelon@users.sourceforge.net
+ *
+ *  Based on the research project "Logbus" by
+ *
+ *  Dipartimento di Informatica e Sistemistica
+ *  University of Naples "Federico II"
+ *  via Claudio, 21
+ *  80121 Naples, Italy
+ *
+ *  Software is distributed under Microsoft Reciprocal License
+ *  Documentation under Creative Commons 3.0 BY-SA License
+*/
+
+using System;
+using System.Diagnostics;
+
+namespace It.Unina.Dis.Logbus.FFDA
+{
+    /// <summary>
+    /// FFDA-information related to Syslog messages
+    /// </summary>
+    public class FFDAInformation
+    {
+        /// <summary>
+        /// Initializes a new instance of FFDAInformation
+        /// </summary>
+        public FFDAInformation() { }
+
+        /// <summary>
+        /// Initializes a new instance of FFDAInformation
+        /// </summary>
+        /// <param name="message">Syslog message to analyze</param>
+        /// <exception cref="System.InvalidOperationException">Message is not FFDA</exception>
+        public FFDAInformation(SyslogMessage message)
+        {
+            try
+            {
+                if (message.MessageId == "FFDA")
+                {
+                    string txt = message.Text;
+                    if (txt == null) throw new InvalidOperationException("Message is not FFD");
+
+                    SyslogAttributes advancedAttrs = message.GetAdvancedAttributes();
+
+                    Host = message.Host;
+                    Process = message.ProcessID ?? message.ApplicationName;
+                    Logger = advancedAttrs.LogName;
+                    
+
+                    string prefix = txt.Substring(0, 3);
+                    if (txt[3] == '-' && txt.Length > 4)
+                    {
+                        FlowId = txt.Substring(4);
+                    }
+
+                    Event = (FFDAEvent)Enum.Parse(typeof(FFDAEvent), prefix.ToUpper());
+                }
+            }
+            catch (InvalidOperationException)
+            { throw; }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Message is not FFD", ex);
+            }
+        }
+
+        /// <summary>
+        /// Host that generated the message
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// Process ID (or name, if ID not specified) that generated the message
+        /// </summary>
+        public string Process { get; set; }
+
+        /// <summary>
+        /// Logger that generated the message
+        /// </summary>
+        public string Logger { get; set; }
+
+        /// <summary>
+        /// Identification of control flow
+        /// </summary>
+        public string FlowId { get; set; }
+
+        /// <summary>
+        /// Event type
+        /// </summary>
+        public FFDAEvent Event { get; set; }
+    }
+}
---
+++ It.Unina.Dis.Logbus.Extensions/FFDA/SyslogMessageExtensions.cs
@@ -0,0 +1,38 @@
+﻿/*
+ *                  Logbus-ng project
+ *    ©2010 Logbus Reasearch Team - Some rights reserved
+ *
+ *  Created by:
+ *      Vittorio Alfieri - vitty85@users.sourceforge.net
+ *      Antonio Anzivino - djechelon@users.sourceforge.net
+ *
+ *  Based on the research project "Logbus" by
+ *
+ *  Dipartimento di Informatica e Sistemistica
+ *  University of Naples "Federico II"
+ *  via Claudio, 21
+ *  80121 Naples, Italy
+ *
+ *  Software is distributed under Microsoft Reciprocal License
+ *  Documentation under Creative Commons 3.0 BY-SA License
+*/
+
+namespace It.Unina.Dis.Logbus.FFDA
+{
+    /// <summary>
+    /// Extensions for <see cref="It.Unina.Dis.Logbus.SyslogMessage"/>
+    /// </summary>
+    public static class SyslogMessageExtensions
+    {
+        /// <summary>
+        /// Gets FFDA information, if message is FFD
+        /// </summary>
+        /// <param name="msg">Syslog message to parse</param>
+        /// <returns>FFDA information about the message</returns>
+        /// <exception cref="System.InvalidOperationException">Message is not FFD</exception>
+        public static FFDAInformation GetFfdaInformation(this SyslogMessage msg)
+        {
+            return new FFDAInformation(msg);
+        }
+    }
+}
